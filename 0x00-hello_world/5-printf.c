2261a6b60a5d6bc3603375806cb8d84e07b202ad
Josephmandel <Josephdogara2@gmail.com>
2022-07-02 16:42:47 -0700

    sdf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry mode
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+		return (0);
+}
