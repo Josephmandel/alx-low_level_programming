7243d759f3ffc8e242a9de6b0040dc8eae3d4cc2
Josephmandel <Josephdogara2@gmail.com>
2022-07-02 17:07:30 -0700

    deff

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,21 @@
-#include <stdio.h>                                                                                                          
-/**                                                                                                                         
- * main - Entry point                                                                                                       
- * Return: Always 0 (Success)                                                                                               
- */                                                                                                                         
-int main(void)                                                                                                              
-{                                                                                                                           
-        char c;                                                                                                             
-        int i;                                                                                                              
-        long int li;                                                                                                        
-        long long ll;                                                                                                       
-        float f;                                                                                                            
-                                                                                                                            
-        printf("Size of a char: %ld byte(s)\n", (unsigned int) sizeof(c));                                                  
-        printf("Size of an int: %ld byte(s)\n", (unsigned int) sizeof(i));                                                  
-        printf("Size of a long int: %ld byte(s)\n", (unsigned long) sizeof(li));                                            
-        printf("Size of a long long int: %ld byte(s)\n", (unsigned long) sizeof((                                           
-ll));                                                                                                                       
-        printf("Size of a float: %ld byte(s)\n", (unsigned int) sizeof(f));                                                 
-        return (0);                                                                                                         
-}  
+#include <stdio.h>
+/**
+ * main - entry mode
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long li;
+	float f;
+
+	printf("Size of a char: %ld byte(s)\n", (unsigned int) sixeof(c)):
+	printf("Size of an int: %ld byte(s)\n", (unsigned int) sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", (unsigned long) sizeof(li));
+	printf("Size of a long int: %ld byte(s)\n", (unsigned long) sizeof(li));
+	printf("Size of a float: %ld byte(s)\n", (unsigned i
+nt) sizeof(f));
+	return (0);
+}
