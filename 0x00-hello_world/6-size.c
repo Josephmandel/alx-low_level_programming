2eb7c9fbc56db62e747eef19b7dd292e0fc5f75f
Josephmandel <Josephdogara2@gmail.com>
2022-07-03 02:35:45 -0700

    tyt

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
-/**
- * main - entry mode
- * Return: Always 0 (Success)
+#include <stdlib.h>
+/*
+ * main - entry mode 
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 	char c;
 	int i;
 	long int li;
-	long long li;
+	long long ll;
 	float f;
 
-	printf("Size of a char: %ld byte(s)\n", (unsigned int) sixeof(c)):
-	printf("Size of an int: %ld byte(s)\n", (unsigned int) sizeof(i));
-	printf("Size of a long int: %ld byte(s)\n", (unsigned long) sizeof(li));
-	printf("Size of a long int: %ld byte(s)\n", (unsigned long) sizeof(li));
-	printf("Size of a float: %ld byte(s)\n", (unsigned i
-nt) sizeof(f));
+	printf("Size of a char: %ld byte(s)",(unsigned int) sizeof(c));
+	printf("Size of an int: %ld byte(s)", (unsigned int) sizeof(i));
+	printf("Size of a long int: %ld byte(s)", (unsigned long) sizeof(li));
+	printf("Size of a long long: %ld byte(s)", (unsigned long sizeof(ll));
+	printf("Size of a float: %ld byte(s)", (unsigned int) sizeof(f));
+
 	return (0);
-}
+	}
